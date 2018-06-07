d9c9ca0c5d5d82cca46f131a0f3703c7eed406e3
Christian Stein <sormuras@gmail.com>
2018-06-07 12:31:58 +0200

    Move sors-test-module-processor into a dedicated repository

--- sors-test-module-processor/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module de.sormuras.sandbox.temopro {
-  requires java.compiler;
-
-  exports de.sormuras.sandbox.temopro;
-
-  provides javax.annotation.processing.Processor with
-      de.sormuras.sandbox.temopro.TestModuleProcessor;
-}
