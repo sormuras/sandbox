737a2a9b68340b40b845c53d66afcb305f662fbc
Christian Stein <sormuras@gmail.com>
2019-02-04 19:39:04 +0100

    Use String as value() return type with RetentionPolicy.RUNTIME works.

--- sors-api-fun/src/Api.java
+++ sors-api-fun/src/Api.java
@@ -2,9 +2,9 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.RUNTIME)
 @Documented
 @interface Api {
-  Status value();
+  String value() default "B";
   enum Status { A, B, C }
 }
--- sors-api-fun/src/Test.java
+++ sors-api-fun/src/Test.java
@@ -4,7 +4,7 @@ import java.lang.annotation.RetentionPolicy;
 /**
  * Javadoc for Test should show Api annotation with status.
  */
-@Api(Api.Status.B)
+@Api("B")
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test {
 }
