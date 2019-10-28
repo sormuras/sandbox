1b651502b45966ffc61192f97fbb6ae4f1fdaaee
Christian Stein <sormuras@gmail.com>
2019-10-28 11:21:24 +0100

    Add java-compiled-versions project

---
+++ java-compiled-versions/Build.java
@@ -0,0 +1,18 @@
+import java.util.spi.ToolProvider;
+
+class Build {
+  public static void main(String[] args) {
+    run("javac", "--version");
+    run("javac", "-d", "bin/classes", "--module-source-path", "src", "--module", "foo");
+    run("jar", "--create", "--file", "bin/foo.jar", "--module-version", "1.0", "-C", "bin/classes/foo", ".");
+    run("jdeps", "--module-path", "bin", "--check", "foo");
+    run("jar", "--describe-module", "--file", "bin/foo.jar");
+  }
+
+  static void run(String name, String... args) {
+    System.out.printf(">> %s %s%n", name, String.join(" ", args));
+    var tool = ToolProvider.findFirst(name).orElseThrow();
+    var code = tool.run(System.out, System.err, args);
+    if (code != 0) throw new Error("Non-zero exit code: " + code);
+  }
+}
